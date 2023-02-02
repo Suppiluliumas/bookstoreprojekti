d8acd7769bc3d7bc9ffe876029cc89e6b0c85f46
Jesse Nevalainen <jesse.nevalainen@myy.haaga-helia.fi>
2023-02-02 17:06:46 +0200

    ekat lisäykset

---
+++ src/main/java/backend/chapter4/bookstore/web/BookController.java
@@ -0,0 +1,8 @@
+package backend.chapter4.bookstore.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class BookController {
+	@RequestMapping(value="/index", method= RequestMethod.GET)
+	
+}
