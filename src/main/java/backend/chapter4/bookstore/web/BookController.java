81bf7fa85058b8a515c586d66b01aed91f45e944
Jesse N <jesse.nevalainen@myy.haaga-helia.fi>
2023-02-10 17:24:59 +0200

    Updated Controller and thymeleaf template

--- src/main/java/backend/chapter4/bookstore/web/BookController.java
+++ src/main/java/backend/chapter4/bookstore/web/BookController.java
@@ -3,18 +3,22 @@ package backend.chapter4.bookstore.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import backend.chapter4.bookstore.domain.Book;
+import backend.chapter4.bookstore.domain.BookRepository;
 @Controller
 public class BookController {
-	@RequestMapping(value="/index", method= RequestMethod.GET)
-	public String getBooks(Model model) {
-		List<Book> books = new ArrayList<Book>();
-		model.addAttribute("index", books);
+	@Autowired
+	private BookRepository repository;
+	
+	@RequestMapping(value="/booklist", method= RequestMethod.GET)
+	public String bookList(Model model) {
+		model.addAttribute("books",repository.findAll());
 		return "booklist";
 	}
 }
