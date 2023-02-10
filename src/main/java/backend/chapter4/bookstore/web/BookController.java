4574ebf2c78d2d9f5d613b5ca24dc2e910bed24c
Jesse N <jesse.nevalainen@myy.haaga-helia.fi>
2023-02-10 19:16:56 +0200

    Added edit button and functionality

--- src/main/java/backend/chapter4/bookstore/web/BookController.java
+++ src/main/java/backend/chapter4/bookstore/web/BookController.java
@@ -1,14 +1,11 @@
 package backend.chapter4.bookstore.web;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import backend.chapter4.bookstore.domain.Book;
 import backend.chapter4.bookstore.domain.BookRepository;
@@ -26,6 +23,11 @@ public class BookController {
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
 		return "addbook";
+	}
+	@RequestMapping(value="/edit/{id}", method = RequestMethod.GET)
+	public String editBook(@PathVariable Long id, Model model) {
+		model.addAttribute("book", repository.findById(id));
+		return "editbook";
 	}
 	
 	@RequestMapping(value= "/save", method = RequestMethod.POST)
