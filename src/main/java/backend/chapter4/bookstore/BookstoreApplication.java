7a1ba2c7371c58e680dbcb1d0104ac2ed33c85cd
Jesse N <jesse.nevalainen@myy.haaga-helia.fi>
2023-02-24 14:18:39 +0200

    added category entity

--- src/main/java/backend/chapter4/bookstore/BookstoreApplication.java
+++ src/main/java/backend/chapter4/bookstore/BookstoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 
 import backend.chapter4.bookstore.domain.Book;
 import backend.chapter4.bookstore.domain.BookRepository;
+import backend.chapter4.bookstore.domain.Category;
+import backend.chapter4.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -15,11 +17,24 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 @Bean
-public CommandLineRunner demo(BookRepository bookRepository) {
+public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
 	return (args) -> {
-		Book b1 = new Book("Sinuhe egyptiläinen", "Mika Waltari", 1945, 978, 29.90);
-		Book b2 = new Book("Mikael Karvajalka", "Mika Waltari", 1948, 951, 24.90);
-		Book b3 = new Book("Silmarillion", "J.R.R Tolkien", 1977, 9516, 39.90);
+		Category category1 = new Category("Scifi");
+		Category category2 = new Category("Comic");
+		Category category3 = new Category("Novel");
+		Category category4 = new Category("Historical fiction");
+		Category category5 = new Category("Fantasy");
+		
+		categoryRepository.save(category1);
+		categoryRepository.save(category2);
+		categoryRepository.save(category3);
+		categoryRepository.save(category4);
+		categoryRepository.save(category5);
+		
+		
+		Book b1 = new Book("Sinuhe egyptiläinen", "Mika Waltari", 1945, 978, 29.90, category4);
+		Book b2 = new Book("Mikael Karvajalka", "Mika Waltari", 1948, 951, 24.90, category4);
+		Book b3 = new Book("Silmarillion", "J.R.R Tolkien", 1977, 9516, 39.90, category5);
 		
 		bookRepository.save(b1);
 		bookRepository.save(b2);
--- src/main/java/backend/chapter4/bookstore/domain/Book.java
+++ src/main/java/backend/chapter4/bookstore/domain/Book.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 
 @Entity
@@ -19,17 +21,22 @@ public class Book {
 	private int isbn;
 	private double price;
 	
+	@ManyToOne
+	@JoinColumn(name= "categoryid")
+	private Category category;
+	
 	public Book() {
 
 	}
 	
-	public Book(String title, String author, int year, int isbn, double price) {
+	public Book(String title, String author, int year, int isbn, double price, Category category) {
 		super();
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
+		this.category = category;
 	}
 	public long getId() {
 		return id;
@@ -67,10 +74,31 @@ public class Book {
 	public void setPrice(double price) {
 		this.price = price;
 	}
+	
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	@Override
+	public String toString() {
+		if (this.category != null)
+			return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + ", category=" + this.getCategory() + "]";
+		else
+			return "Book [id=" + id + ", title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn
+					+ ", price=" + price + "]";
+	}
+
+	
+	}
 
 
 	
-}
+
 	
 	
 	
