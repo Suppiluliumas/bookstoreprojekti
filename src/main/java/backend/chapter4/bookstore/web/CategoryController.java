a2472691372e3c9c0d9666cc208ca9e2389775ce
Jesse N <jesse.nevalainen@myy.haaga-helia.fi>
2023-02-24 15:35:12 +0200

    added create functionality to categories

---
+++ src/main/java/backend/chapter4/bookstore/web/CategoryController.java
@@ -0,0 +1,34 @@
+package backend.chapter4.bookstore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import backend.chapter4.bookstore.domain.Book;
+import backend.chapter4.bookstore.domain.Category;
+import backend.chapter4.bookstore.domain.CategoryRepository;
+
+
+@Controller
+public class CategoryController {
+	@Autowired
+	private CategoryRepository repository;
+	
+	@RequestMapping(value="/categorylist", method= RequestMethod.GET)
+	public String categoryList(Model model) {
+		model.addAttribute("categories", repository.findAll());
+		return "categorylist";
+	}
+	@RequestMapping(value="/addcategory")
+	public String addBook(Model model) {
+		model.addAttribute("category", new Category());
+		return "addcategory";
+}
+	@RequestMapping(value= "/savecategory", method = RequestMethod.POST)
+	public String save(Category category) {
+		repository.save(category);
+		return "redirect:categorylist";
+	}
+}
