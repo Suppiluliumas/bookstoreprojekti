d610f35dfc2ea43c2ea31fc34da3d803f1c2299a
Jesse N <jesse.nevalainen@myy.haaga-helia.fi>
2023-03-17 14:08:01 +0200

    application.properties modified

--- src/main/java/backend/chapter4/bookstore/WebSecurityConfig.java
+++ src/main/java/backend/chapter4/bookstore/WebSecurityConfig.java
@@ -1,7 +1,5 @@
 package backend.chapter4.bookstore;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
--- src/main/java/backend/chapter4/bookstore/web/CategoryController.java
+++ src/main/java/backend/chapter4/bookstore/web/CategoryController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import backend.chapter4.bookstore.domain.Book;
+
 import backend.chapter4.bookstore.domain.Category;
 import backend.chapter4.bookstore.domain.CategoryRepository;
 
